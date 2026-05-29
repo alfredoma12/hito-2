efb361e91e535eb6b359f22489314b549cd51fbf
alfredo <alfredomunoz0696@gmail.com>
2026-05-29 10:49:46 -0400

    sera?

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/marketplace-app">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
